ccb1bdae8ff1caff1720113d0354e015091a0b59
gxvl <geraldo.goncalves@ufrpe.br>
2024-06-24 17:01:23 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+// `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
 
-function App() {
+import { useEffect, useState } from "react";
+
+export default function App() {
+  const [entry, setEntry] = useState();
+  const [first, setFirst] = useState("EUR");
+  const [second, setSecond] = useState("USD");
+  const [result, setResult] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(
+    function () {
+      async function convert() {
+        setIsLoading(true);
+        const res = await fetch(
+          `https://api.frankfurter.app/latest?amount=${entry}&from=${first}&to=${second}`
+        );
+        const data = await res.json();
+        setResult(data.rates[second]);
+        setIsLoading(false);
+      }
+      if (first === second) return setResult(entry);
+      convert();
+    },
+    [entry, first, second]
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        type="text"
+        value={entry}
+        onChange={(e) => setEntry(Number(e.target.value))}
+        disabled={isLoading}
+      />
+      <select
+        value={first}
+        onChange={(e) => setFirst(e.target.value)}
+        disabled={isLoading}
+      >
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <select
+        value={second}
+        onChange={(e) => setSecond(e.target.value)}
+        disabled={isLoading}
+      >
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <p>{result}</p>
     </div>
   );
 }
-
-export default App;
